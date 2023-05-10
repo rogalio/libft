62d4ab37b08e479e3e8c5babc090b7fa1753517a
rogalio <96726858+rogalio@users.noreply.github.com>
2023-05-10 20:51:12 +0200

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,17 +12,18 @@
 
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
-	size_t	len;
+	const size_t	n = ft_strlen(s1);
+	char			*str;
+	size_t			len;
 
-	len = ft_strlen(s1) + ft_strlen(s2);
+	len = n + ft_strlen(s2);
 	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
-	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
-	ft_strlcat(str, s2, len + 1);
+	ft_strlcpy(str, s1, n + 1);
+	ft_strlcpy(str + n, s2, len + 1);
 	return (str);
 }
 
