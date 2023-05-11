d25efbe8417cd4327ffe3d63108973bc76a19e4f
rogalio <96726858+rogalio@users.noreply.github.com>
2023-05-11 12:55:06 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -26,3 +26,18 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	return (0);
 }
 
+/**
+ * @file
+ * @brief      Compare lexicographiquement les n premiers caractères de deux chaînes de caractères.
+ *
+ * @param[in]  s1        Première chaîne de caractères à comparer.
+ * @param[in]  s2        Deuxième chaîne de caractères à comparer.
+ * @param[in]  n         Nombre de caractères à comparer.
+ *
+ * @return     Un entier inférieur, égal, ou supérieur à zéro, si s1 est respectivement inférieure, égale ou supérieure à s2.
+ *
+ * @details    La fonction ft_strncmp procède comme suit :
+ *             1. Itère sur les deux chaînes de caractères jusqu'à atteindre le nombre de caractères spécifié par n ou jusqu'à atteindre la fin de l'une des deux chaînes.
+ *             2. Compare chaque caractère des deux chaînes de caractères. Si un caractère différent est trouvé, retourne la différence entre les valeurs des caractères.
+ *             3. Si aucun caractère différent n'est trouvé après la comparaison des n caractères ou à la fin des chaînes de caractères, retourne 0 pour indiquer que les chaînes sont égales.
+ */
