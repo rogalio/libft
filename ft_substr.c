f02263c783ca60a8f81755ff6373eb8ba8a03fc4
rogalio <96726858+rogalio@users.noreply.github.com>
2023-05-12 13:01:07 +0200

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -20,19 +20,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
     if (!s)
         return NULL;
-
     s_len = ft_strlen(s);
-
     if (start >= s_len)
         return ft_strdup("");
-
     if (len > s_len - start)
         len = s_len - start;
-
     str = malloc(sizeof(char) * (len + 1));
     if (!str)
         return NULL;
-
     i = 0;
     while (s[start] && i < len)
         str[i++] = s[start++];
